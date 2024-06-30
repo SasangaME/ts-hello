297d9c3cdada03b2a795c1b6aab1cb6a9cf5a79e
sasangame <madhumaledirisinghe@gmail.com>
2024-06-30 10:45:48 +0530

    anonymous function

--- src/basics/functions.ts
+++ src/basics/functions.ts
@@ -5,6 +5,7 @@ export function demo() {
         const random = await getRandomInt(1, 100);
         console.log(`random : ${random}`);
     })();
+    anonymous();
 }
 
 async function getRandomInt(min: number, max: number): Promise<number> {
@@ -12,3 +13,11 @@ async function getRandomInt(min: number, max: number): Promise<number> {
 }
 
 // anonymous functions
+function anonymous() {
+    const names = ["Sarala", "Sasanga", "Darshana"]
+
+    names.forEach(function (name)
+    {
+        console.log(name.toUpperCase());
+    })
+}
