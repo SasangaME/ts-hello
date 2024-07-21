1430c456c5aa139503fbc5d3b7dc16b1f7b3b377
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2024-07-21 13:58:44 +0530

    intersection

--- src/basics/intersection.ts
+++ src/basics/intersection.ts
@@ -18,3 +18,9 @@ interface Circle {
 
 type ColorfulCircle = Colorful & Circle;
 
+/**
+ * `interface`s allowed us to build up new types from other types by extending them. TypeScript provides another construct 
+ * intersection types that is mainly used to combine existing object types.
+
+ * An intersection type is defined using the `&` operator.
+ */
